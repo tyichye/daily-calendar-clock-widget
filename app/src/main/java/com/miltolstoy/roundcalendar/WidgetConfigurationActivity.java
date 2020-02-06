3020a4a52fa28831e629d736601624fe0e216b1f
miltolstoy <miltolstoy@gmail.com>
2020-02-06 18:31:19 +0200

    refactor: add widget configuration activity to request calendar permissions

---
+++ app/src/main/java/com/miltolstoy/roundcalendar/WidgetConfigurationActivity.java
@@ -0,0 +1,79 @@
+package com.miltolstoy.roundcalendar;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProviderInfo;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Point;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.RemoteViews;
+
+import static com.miltolstoy.roundcalendar.Logging.TAG;
+
+public class WidgetConfigurationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_widget_configuration);
+
+        CalendarAdapter calendarAdapter = new CalendarAdapter(this);
+        calendarAdapter.requestCalendarPermissionsIfNeeded();
+
+        int appWidgetId = getAppWidgetId(getIntent());
+        if (appWidgetId == AppWidgetManager.INVALID_APPWIDGET_ID) {
+            sendResultAndExit(RESULT_CANCELED, appWidgetId);
+        }
+
+        AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(this);
+        RemoteViews views = new RemoteViews(getPackageName(), R.layout.widget);
+
+        Point widgetSize = getWidgetSize(this, appWidgetManager, appWidgetId);
+        drawWidget(this, views, widgetSize);
+        appWidgetManager.updateAppWidget(appWidgetId, views);
+
+        sendResultAndExit(RESULT_OK, appWidgetId);
+    }
+
+
+    public static void drawWidget(Context context, RemoteViews views, Point widgetSize) {
+        CalendarAdapter calendarAdapter = new CalendarAdapter(context);
+        ClockView clockView = new ClockView(context, widgetSize);
+        clockView.setCalendarAdapter(calendarAdapter);
+        Bitmap bitmap = Bitmap.createBitmap(widgetSize.x, widgetSize.y, Bitmap.Config.ARGB_8888);
+        clockView.draw(new Canvas(bitmap));
+        views.setImageViewBitmap(R.id.widgetClockView, bitmap);
+    }
+
+    public static Point getWidgetSize(Context context, AppWidgetManager appWidgetManager, int appWidgetId) {
+        AppWidgetProviderInfo widgetInfo = appWidgetManager.getAppWidgetInfo(appWidgetId);
+        int width = pxToDp(context, widgetInfo.minWidth);
+        int height = pxToDp(context, widgetInfo.minHeight);
+        return new Point(width, height);
+    }
+
+
+    private int getAppWidgetId(Intent intent) {
+        Bundle extras = intent.getExtras();
+        if (extras == null) {
+            Log.e(TAG, "Empty extras");
+            return AppWidgetManager.INVALID_APPWIDGET_ID;
+        }
+        return extras.getInt(AppWidgetManager.EXTRA_APPWIDGET_ID, AppWidgetManager.INVALID_APPWIDGET_ID);
+    }
+
+    private void sendResultAndExit(int result, int appWidgetId) {
+        Intent resultValue = new Intent();
+        resultValue.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId);
+        setResult(result, resultValue);
+        finish();
+    }
+
+    private static int pxToDp(Context context,int dp) {
+        return dp / (int) context.getResources().getDisplayMetrics().density;
+    }
+}
--- app/src/main/java/com/miltolstoy/roundcalendar/WidgetProvider.java
+++ app/src/main/java/com/miltolstoy/roundcalendar/WidgetProvider.java
@@ -2,15 +2,9 @@ package com.miltolstoy.roundcalendar;
 
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
-import android.appwidget.AppWidgetProviderInfo;
 import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.Canvas;
 import android.graphics.Point;
-import android.util.Log;
 import android.widget.RemoteViews;
-
-import static com.miltolstoy.roundcalendar.Logging.TAG;
 
 
 public class WidgetProvider extends AppWidgetProvider {
@@ -19,24 +13,10 @@ public class WidgetProvider extends AppWidgetProvider {
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget);
 
-        AppWidgetProviderInfo widgetInfo = appWidgetManager.getAppWidgetInfo(appWidgetIds[0]);
-        int width = pxToDp(context, widgetInfo.minWidth);
-        int height = pxToDp(context, widgetInfo.minHeight);
-
-        CalendarAdapter calendarAdapter = new CalendarAdapter(context);
-        calendarAdapter.requestCalendarPermissionsIfNeeded();
-
-        ClockView clockView = new ClockView(context, new Point(width, height));
-        clockView.setCalendarAdapter(calendarAdapter);
-
-        Bitmap bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
-        clockView.draw(new Canvas(bitmap));
-        views.setImageViewBitmap(R.id.widgetClockView, bitmap);
-        appWidgetManager.updateAppWidget(appWidgetIds[0], views);
-
-    }
-
-    private int pxToDp(Context context, int dp) {
-        return dp / (int) context.getResources().getDisplayMetrics().density;
+        for (int id : appWidgetIds) {
+            Point widgetSize = WidgetConfigurationActivity.getWidgetSize(context, appWidgetManager, id);
+            WidgetConfigurationActivity.drawWidget(context, views, widgetSize);
+            appWidgetManager.updateAppWidget(id, views);
+        }
     }
 }
